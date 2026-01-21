03739ff1bf2bb989879d594ffe007cd4bfbf8d31
ujjwalgupta132004 <ujjwalgupta1309@gmail.com>
2026-01-21 18:29:07 +0530

    utils code

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}))
+app.use(cookieParser())
 
 
 app.use(express.json({ limit: "16kb" }))
